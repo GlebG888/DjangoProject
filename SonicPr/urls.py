cb4e11443bd68fc47e3fddbeaf27d235d28eb366
GlebG888 <gleb_gro@mail.ru>
2022-12-17 12:41:55 +0300

    Добавлен новый проект

---
+++ SonicPr/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from MainApp import views
+
+urlpatterns = [
+    path('test/', views.test),
+    path('',views.home),
+    path('about/',views.about),
+    path('items/',views.items_list),
+    path('item/<int:id>',views.item_page),
+
+
+]
